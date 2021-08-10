19baf4d1d97b93b423c806d0be86e3b057c9379f
Thrinadh Bheri <trinadha.bheri@thomsonreuters.com@thomsonreuters.com>
2021-08-10 23:11:24 +0530

    est

--- tf/s3.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_s3_bucket" "a205923-thrinadh-codbuild" {
-  bucket = "a205923-thrinadh-codbuild"
-  acl    = "private"
-  tags = local.common_tags
-  
-}
--- tf/var_local.tf
+++
@@ -1,9 +0,0 @@
-locals {
-
-  common_tags = {
-      "tr:application-asset-insight-id" ="205923"
-      "tr:financial-identifier"	= "66497"
-      "tr:environment-name"	= "tf-poc"
-      "tr:project-name"	= "akkadia-u6111359"
-  }
-}
